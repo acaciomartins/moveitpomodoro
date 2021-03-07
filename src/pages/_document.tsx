5baa6656c920c0a265fb94bfa681aec3630b746a
acaciomartins <acacio.am@gmail.com>
2021-03-07 15:49:48 -0300

    Inclusao Google Adsense

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,19 +3,29 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 export default class MyDocument extends Document {
     render() {
         return (
-        <Html>
-            <Head>
-                <link rel="shortcut icon" href="favicon.png" type="image/png" />
-                
-                <link rel="preconnect" href="https://fonts.gstatic.com" />
-                <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet" />
-                <script data-ad-client="ca-pub-4698968557005948" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-            </Head>
-            <body>
-                <Main />
-                <NextScript />
-            </body>
-        </Html>
+            <Html>
+                <Head>
+                    <link rel="shortcut icon" href="favicon.png" type="image/png" />
+
+                    <link rel="preconnect" href="https://fonts.gstatic.com" />
+                    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet" />
+                    <script data-ad-client="ca-pub-4698968557005948" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `
+                        (adsbygoogle = window.adsbygoogle || []).push({
+                            google_ad_client: "6885921580",
+                            enable_page_level_ads: true
+                        });
+                            `
+                        }}
+                    />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
         );
     }
 }
